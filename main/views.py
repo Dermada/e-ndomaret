471af6e1c6b11e45d5b5dc625241d84207a1ccc9
Dermada <141832231+Dermada@users.noreply.github.com>
2024-09-11 00:11:51 +0700

    Telah Membuat model dan membuat fungsi views

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'app_name': 'E-Ndomaret',  
+        'my_name': 'Muhammad Daffa Abyaz Tjiptadi',        
+        'my_class': 'PBP A',      
     }
-
     return render(request, "main.html", context)
+
